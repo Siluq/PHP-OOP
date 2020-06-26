f7302adaa894f06d45ea8966c7932a6ed2c666c8
Sam Boesjes <534268@student.glu.nl>
2020-06-26 23:51:03 +0200

    DONE

--- eindopdracht/index.php
+++ eindopdracht/index.php
@@ -1,31 +1,34 @@
 <?php 
 
-    //require all external files once
+    //Alle classes worden toegevoegd aan index
     require_once('figuur.class.php');
     require_once('cilinder.class.php');
     require_once('vierkant.class.php');
     
-    // Objects from the exersice
-    $mijn_cilinder = new Cilinder(10, 3);
-    $mijn_cilinder->setOmschrijving("Cilinder");
+    // Objecten worden gemaakt van opdracht
     $mijn_vierkant = new Vierkant(5);
     $mijn_vierkant->setOmschrijving("vierkant");
-    
-    // Made up objects
-    $mijn_tweede_cilinder = new Cilinder(18, 4);
-    $mijn_tweede_vierkant = new Vierkant(12);
+    $mijn_cilinder = new Cilinder(10, 3);
+    $mijn_cilinder->setOmschrijving("Cilinder");  
 
-    // echo objects
-    echo $mijn_cilinder->getOmschrijving();
-    echo "<br>";
-    echo $mijn_cilinder->berekenOppervlakte();
-    echo "<br><br>";
+    // Objecten worden op beeld weergegeven
     echo $mijn_vierkant->getOmschrijving();
     echo "<br>";
     echo $mijn_vierkant->berekenOppervlakte();
-    echo "<br><br><hr>De 2 objecten met zelf verzonnen afmetingen<hr>";
-    echo $mijn_tweede_cilinder->berekenOppervlakte();
+    echo "<br><br>";
+    echo $mijn_cilinder->getOmschrijving();
     echo "<br>";
+    echo $mijn_cilinder->berekenOppervlakte();
+
+
+    // De 2 zelfgemaakte figuren
+    $mijn_tweede_vierkant = new Vierkant(12);
+    $mijn_tweede_cilinder = new Cilinder(18, 4);
+    
+    // Zelf gemaakte objecten worden op beeld weergegeven
+    echo "<br><br><hr>De 2 objecten met zelf verzonnen afmetingen<hr>";
     echo $mijn_tweede_vierkant->berekenOppervlakte();
+    echo "<br>";
+    echo $mijn_tweede_cilinder->berekenOppervlakte();
 
 ?>
